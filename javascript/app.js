ca8a4188a7dc9f8d5f097d75c1cad24d4564d1cb
breal5000 <34284810+breal5000@users.noreply.github.com>
2018-04-11 11:10:44 -0700

    more changes

--- javascript/app.js
+++ javascript/app.js
@@ -23,13 +23,24 @@ database.ref().on("child_added", function(childSnap) {
     var min = childSnap.val().min;
     var next = childSnap.val().next;
 
-    $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + next + "</td><td>" + min + "</td></tr>");
+    
+    var tableRow = $("<tr>");
+    tableRow.append("<td>"+ name + "</td>");
+    $("tbody").append(tableRow);
+    tableRow.append("<td>"+ destination + "</td>");
+    $("tbody").append(tableRow);
+    tableRow.append("<td>"+ frequency + "</td>");
+    $("tbody").append(tableRow);
+    tableRow.append("<td>"+ next + "</td>");
+    $("tbody").append(tableRow);
+    tableRow.append("<td>"+ min + "</td></tr>");
+    $("tbody").append(tableRow);
 });
 
 database.ref().on("value", function(snapshot) {
 });
 
-//grabs information from the form
+
 $("#addTrainBtn").on("click", function() {
 
     var trainName = $("#trainNameInput").val().trim();
@@ -38,30 +49,33 @@ $("#addTrainBtn").on("click", function() {
     var frequency = $("#frequencyInput").val().trim();
 
     //ensures that each input has a value
-    if (trainName == "") {
-        alert('Enter a train name.');
-        return false;
-    }
-    if (destination == "") {
-        alert('Enter a destination.');
-        return false;
-    }
-    if (firstTrain == "") {
-        alert('Enter a first train time.');
-        return false;
-    }
-    if (frequency == "") {
-        alert('Enter a frequency');
-        return false;
-    }
+    //if (trainName == "") {
+      //  alert('Enter a train name.');
+        //return false;
+    //}
+    //if (destination == "") {
+      //  alert('Enter a destination.');
+        //return false;
+    //}
+    //if (firstTrain == "") {
+      //  alert('Enter a first train time.');
+        //return false;
+    //}
+    //if (frequency == "") {
+      //  alert('Enter a frequency');
+        //return false;
+    //}
 
-    // THE MATH!
-    //subtracts the first train time back a year to ensure it's before current time.
+
+    //go back 1 year to make sure it's before current time.
     var firstTrainConverted = moment(firstTrain, "hh:mm").subtract("1, years");
-    // the time difference between current time and the first train
+    // time difference between current time and the first train
     var difference = currentTime.diff(moment(firstTrainConverted), "minutes");
+    // use modolo to assign the time between first and next train
     var remainder = difference % frequency;
+    // uses remainder to calculate next train
     var minUntilTrain = frequency - remainder;
+    // calculates time until next train
     var nextTrain = moment().add(minUntilTrain, "minutes").format("hh:mm a");
 
     var newTrain = {
@@ -81,12 +95,6 @@ $("#addTrainBtn").on("click", function() {
     $("#firstInput").val("");
     $("#frequencyInput").val("");
 
-    return false;
+    //return false;
 });
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Employee start date of January 1, 2015
-// Assume current date is March 1, 2016
 
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
